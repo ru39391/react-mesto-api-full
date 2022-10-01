b2ca1d2e4cefca2d32a9d0695c86309e19c5c996
ru39391 <dev@39391.ru>
2022-10-01 20:16:37 +0300

    hotfix/установить корректное значение для порта по умолчанию

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const { createUser, login } = require('./controllers/users');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const { actionMessages, errMessageNotFound, patterUrl } = require('./utils/constants');
 
-const { PORT = 4000 } = process.env;
+const { PORT = 3000 } = process.env;
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
